efabb399e8b4634b31bb77124255649c7b4975b1
Tac Tacelosky <tacman@gmail.com>
2022-08-01 18:42:15 -0400

    switch to 1.1-dev, drop |datatable

--- src/Twig/TwigExtension.php
+++ src/Twig/TwigExtension.php
@@ -34,7 +34,7 @@ class TwigExtension extends AbstractExtension
             // If your filter generates SAFE HTML, you should add a third
             // parameter: ['is_safe' => ['html']]
             // Reference: https://twig.symfony.com/doc/3.x/advanced.html#automatic-escaping
-            new TwigFilter('datatable', [$this, 'datatable'], ['needs_environment' => true, 'is_safe' => ['html']]),
+//            new TwigFilter('datatable', [$this, 'datatable'], ['needs_environment' => true, 'is_safe' => ['html']]),
         ];
     }
 
