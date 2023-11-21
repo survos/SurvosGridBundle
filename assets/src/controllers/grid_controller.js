e0aef15ef6b614fa04c298f407c27022e0a2fa18
tacman <info@survos-ci.com>
2023-11-21 14:21:02 +0000

    remove grid, api-grid only, cleanup

--- assets/src/controllers/grid_controller.js
+++ assets/src/controllers/grid_controller.js
@@ -13,29 +13,8 @@ import {Controller} from "@hotwired/stimulus";
 // import 'bootstrap';
 
 import DataTables from 'datatables.net-bs5'
-import 'datatables.net-bs5/css/dataTables.bootstrap5.min.css';
 
-import 'datatables.net-select-bs5';
-import 'datatables.net-searchpanes-bs5';
-import 'datatables.net-buttons-bs5';
-
-// https://jsfiddle.net/tacman1123/b2f3hj08/23/
-
-// import {default as axios} from "axios";
-import 'datatables.net-searchpanes-bs5/css/searchPanes.bootstrap5.min.css';
-import 'datatables.net-responsive-bs5/css/responsive.bootstrap5.min.css';
-import 'datatables.net-scroller-bs5/css/scroller.bootstrap5.min.css';
-import 'datatables.net-buttons-bs5/css/buttons.bootstrap5.min.css';
-
-// import DataTables from "datatables.net-bs5";
-// @todo: re-add these when importmap works
-import 'datatables.net-select-bs5';
-import 'datatables.net-responsive-bs5';
-// import 'datatables.net-buttons-bs5';
-import 'datatables.net-scroller-bs5';
-// import 'datatables.net-buttons/js/buttons.colVis.min';
-// import 'datatables.net-buttons/js/buttons.html5.min';
-// import 'datatables.net-buttons/js/buttons.print.min';
+import '../datatables-plugins.js';
 
 // import {Modal} from "bootstrap"; !!
 // https://stackoverflow.com/questions/68084742/dropdown-doesnt-work-after-modal-of-bootstrap-imported
